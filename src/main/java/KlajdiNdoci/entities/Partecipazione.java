4936297c923acb2ea952447ffea8c86c8b3b402a
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-10-18 14:17:46 +0200

    created classes

---
+++ src/main/java/KlajdiNdoci/entities/Partecipazione.java
@@ -0,0 +1,63 @@
+package KlajdiNdoci.entities;
+
+import KlajdiNdoci.enums.Stato;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Partecipazione {
+    @Id
+    @GeneratedValue
+    private long id;
+    private Persona persona;
+    private Evento evento;
+    private Stato stato;
+
+    public Partecipazione() {
+    }
+
+    public Partecipazione(Persona persona, Evento evento, Stato stato) {
+        this.persona = persona;
+        this.evento = evento;
+        this.stato = stato;
+    }
+
+    @Override
+    public String toString() {
+        return "Partecipazione{" +
+                "id=" + id +
+                ", persona=" + persona +
+                ", evento=" + evento +
+                ", stato=" + stato +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    public Evento getEvento() {
+        return evento;
+    }
+
+    public void setEvento(Evento evento) {
+        this.evento = evento;
+    }
+
+    public Stato getStato() {
+        return stato;
+    }
+
+    public void setStato(Stato stato) {
+        this.stato = stato;
+    }
+}
---
+++ src/main/java/KlajdiNdoci/entities/Persona.java
@@ -0,0 +1,99 @@
+package KlajdiNdoci.entities;
+
+import KlajdiNdoci.enums.Sesso;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.Set;
+
+public class Persona {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String nome;
+    private String cognome;
+    private String email;
+    private LocalDate dataNascita;
+    private Sesso sesso;
+    private Set<Partecipazione> listaPartecipazioni;
+
+    public Persona() {
+    }
+
+    public Persona(String nome, String cognome, String email, LocalDate dataNascita, Sesso sesso, Set<Partecipazione> listaPartecipazioni) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.dataNascita = dataNascita;
+        this.sesso = sesso;
+        this.listaPartecipazioni = listaPartecipazioni;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", email='" + email + '\'' +
+                ", dataNascita=" + dataNascita +
+                ", sesso=" + sesso +
+                ", listaPartecipazioni=" + listaPartecipazioni +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setDataNascita(LocalDate dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+
+    public Sesso getSesso() {
+        return sesso;
+    }
+
+    public void setSesso(Sesso sesso) {
+        this.sesso = sesso;
+    }
+
+    public Set<Partecipazione> getListaPartecipazioni() {
+        return listaPartecipazioni;
+    }
+
+    public void setListaPartecipazioni(Set<Partecipazione> listaPartecipazioni) {
+        this.listaPartecipazioni = listaPartecipazioni;
+    }
+}
