a49e2b59dd8f6327d43f88fe0ac6104635212c53
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-10-18 14:05:36 +0200

    Initial commit

---
+++ src/main/java/KlajdiNdoci/Application.java
@@ -0,0 +1,45 @@
+package KlajdiNdoci;
+
+import KlajdiNdoci.entities.Evento;
+import KlajdiNdoci.entities.EventoDAO;
+import KlajdiNdoci.enums.TipoEvento;
+import KlajdiNdoci.utils.JpaUtil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.time.LocalDate;
+
+public class Application {
+    private static final EntityManagerFactory emf = JpaUtil.getEntityManagerFactory();
+
+    public static void main(String[] args) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            EventoDAO ed = new EventoDAO(em);
+
+            Evento ev1 = new Evento("Evento1", LocalDate.of(2022, 12, 5), "ciao", TipoEvento.PUBBLICO, 200);
+            ed.save(ev1);
+
+            Evento evFromDB = ed.getById(5);
+            if (evFromDB != null) {
+                System.out.println(evFromDB);
+            }
+            ed.delete(5);
+
+            Evento ev7FromDB = ed.getById(8);
+            if (ev7FromDB != null) {
+                ev7FromDB.setTitolo("Evento7");
+                System.out.println(ev7FromDB);
+                em.refresh(ev7FromDB);
+                System.out.println(ev7FromDB);
+            }
+
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+            em.close();
+            emf.close();
+        }
+
+    }
+}
